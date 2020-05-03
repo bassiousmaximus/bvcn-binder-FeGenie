87b171e3be5620050453330b0a917ecd2918e3dc
Arkadiy-Garber <arkg@udel.edu>
2020-05-02 22:32:10 -0600

    Update FeGenie.py

--- FeGenie/FeGenie.py
+++ FeGenie/FeGenie.py
@@ -1417,10 +1417,11 @@ def main():
                     memoryDict[dataset][orf]["cutoff"] = ls[5]
                     memoryDict[dataset][orf]["clu"] = clu
                     memoryDict[dataset][orf]["heme"] = ls[7]
-                    memoryDict[dataset][orf]["seq"] = ls[10]
+                    memoryDict[dataset][orf]["seq"] = ls[8]
                     if args.ref != "NA":
                         memoryDict[dataset][orf]["blastHit"] = ls[8]
                         memoryDict[dataset][orf]["blastEval"] = ls[9]
+                        memoryDict[dataset][orf]["seq"] = ls[10]
                 else:
                     cat = ls[0]
                     dataset = ls[1]
@@ -1433,10 +1434,11 @@ def main():
                     memoryDict[dataset][orf]["cutoff"] = "evalue-cutoff: 1E-10"
                     memoryDict[dataset][orf]["clu"] = ls[6]
                     memoryDict[dataset][orf]["heme"] = ls[7]
-                    memoryDict[dataset][orf]["seq"] = ls[10]
+                    memoryDict[dataset][orf]["seq"] = ls[8]
                     if args.ref != "NA":
                         memoryDict[dataset][orf]["blastHit"] = ls[8]
                         memoryDict[dataset][orf]["blastEval"] = ls[9]
+                        memoryDict[dataset][orf]["seq"] = ls[10]
 
                     geneToCatDict[hmm] = cat
                     clusterDict[ls[7]].append(hmm + "|" + dataset + "|" + orf)
